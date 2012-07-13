28f2a81fd3bf11b68d271b246e5612ca2e9af337
moe <moe@busyloop.net>
2012-07-13 20:07:38 +0200

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,6 @@
 require 'simplecov'
 SimpleCov.start
 
-RSpec::Matchers.define :have_stdout do |regex|
-  define_method :has_stdout? do |actual|
-    regex = /^#{Regexp.escape(regex)}$/ if regex.is_a?(String)
-
-    $stdout = StringIO.new
-    actual.call
-    $stdout.rewind
-    captured = $stdout.read
-
-    $stdout = STDOUT
-    captured =~ regex
-  end
-  match { |actual| has_stdout?(actual) }
-end
-
-
 def capture_streams(stdin_str = '')
   begin
     require 'stringio'
